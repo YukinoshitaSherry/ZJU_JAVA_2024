1bffbe728883c12bb41175c75c8ee616da84e220
YukinoshitaSherry <1710648201@qq.com>
2024-12-22 00:27:05 +0800

    hw3 init

--- hw4/ChatRoom/src/main/java/components/User.java
+++ hw4/ChatRoom/src/main/java/components/User.java
@@ -2,38 +2,26 @@ package components;
 
 import java.net.Socket;
 
-class User{
-	//用户姓名
-	private String username;
-	//用户性别
-	private String password;
-	public String getUsername() {
-		return username;
-	}
+class User {
+    // 用户姓名
+    private String username;
+    // 用户性别
+    private String password;
+    public String getUsername() { return username; }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setUsername(String username) { this.username = username; }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() { return password; }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	//用户自己的socket
-	private Socket sock;
-	public User(String username,String password,Socket sock){
-		setUsername(username);
-		setPassword(password);
-		setSock(sock);
-	}
-	
-	public Socket getSock(){
-		return sock;
-	}
-	public void setSock(Socket sock){
-		this.sock=sock;
-	}
+    public void setPassword(String password) { this.password = password; }
+    // 用户自己的socket
+    private Socket sock;
+    public User(String username, String password, Socket sock) {
+        setUsername(username);
+        setPassword(password);
+        setSock(sock);
+    }
+
+    public Socket getSock() { return sock; }
+    public void setSock(Socket sock) { this.sock = sock; }
 }
